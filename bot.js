02f83309859de0a38dbe1822f3224e93480d7f76
Michael Westbom <michael@agilion.com>
2016-07-18 21:24:48 -0400

    Improve messaging

--- bot.js
+++ bot.js
@@ -2,10 +2,20 @@ const botBuilder = require('claudia-bot-builder')
 const SlackTemplate = botBuilder.slackTemplate
 const aws = require('aws-sdk')
 const s3 = new aws.S3()
+const cloudfront = new aws.CloudFront()
 
-module.exports = botBuilder(request => {
-  if (request.type === 'slack-slash-command') {
-    const listResponse = s3.listObjects({ Bucket: 'ello-staging-promos' })
+const helpMessage = () => new SlackTemplate(
+`Deploy and control promotion data for the webapp
+
+Commands:
+
+  \`deploy\` - Promotes staging data to production
+  \`invalidate <environment>\` - invalidates the CDN cache for a given environment.
+    Note: Invalidation can take a little time
+`).get()
+
+const deployPromos = () => {
+  const listResponse = s3.listObjects({ Bucket: 'ello-staging-promos' })
 
     const chatPromise = listResponse.promise()
 
@@ -24,13 +34,9 @@ module.exports = botBuilder(request => {
 
       return Promise.all(copyOperations)
         .then(copyResponses => {
-          const originalRequest = request.originalRequest
-          console.log(originalRequest)
 
-          const authorUsername = originalRequest.user_name
           const message = new SlackTemplate('Thanks!')
                 .addAttachment('A1')
-                .addAuthor(authorUsername)
                 .addText('Promos deployed to production')
 
           return message.get()
@@ -40,5 +46,58 @@ module.exports = botBuilder(request => {
       console.log(error)
       return error
     })
+}
+
+const invalidateCache = cdnEnvironment => {
+  if (cdnEnvironment !== 'staging' && cdnEnvironment !== 'production') {
+    return new SlackTemplate(`${cdnEnvironment} is not a valid environment`).get()
   }
-})
+
+  const distributionId = cdnEnvironment === 'staging' ? 'E233P4CE1268H7' : 'ENFF23C7D8VH0'
+
+  var invalidateRequest = cloudfront.createInvalidation({
+    DistributionId: distributionId,
+    InvalidationBatch: {
+      CallerReference: `BotInvalidation${new Date().toJSON()}`,
+      Paths: {
+        Quantity: 1,
+        Items: [ '/*.json' ]
+      }
+    }
+  }).promise()
+
+  return invalidateRequest.then(() => {
+    return new SlackTemplate(`Invalidating ${cdnEnvironment} promos. This may take a little while`).get()
+  }).catch(err => {
+    return new SlackTemplate(`Error! ${err}`).get()
+  })
+}
+
+const responder = request => {
+  const text = request.text
+  const args = text.split(/\W+/)
+
+  const command = args[0]
+
+  switch (command) {
+  case 'help':
+    return helpMessage()
+  case 'deploy':
+    return deployPromos()
+  case 'invalidate':
+    return invalidateCache(args[1])
+  default:
+    return helpMessage()
+  }
+}
+
+const botMainFunction = request => {
+  if (request.type === 'slack-slash-command') {
+    return responder(request)
+  }
+}
+
+exports = module.exports = botBuilder(botMainFunction)
+
+exports.helpMessage = helpMessage
+exports.mainFunction = botMainFunction
